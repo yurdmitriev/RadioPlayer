0c24eba866cf353d60be7038908ec8a86483b594
yurdmitriev <yurdmitriev02@gmail.com>
2022-06-06 03:42:21 +0300

    Clear project

--- app/src/main/java/com/yurdm/radioplayer/MainActivity.kt
+++ app/src/main/java/com/yurdm/radioplayer/MainActivity.kt
@@ -1,51 +1,21 @@
 package com.yurdm.radioplayer
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.github.kittinunf.fuel.Fuel
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.yurdm.radioplayer.databinding.ActivityMainBinding
-import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var adapter: RadioRecyclerAdapter
-    private val list: MutableList<Radio> = mutableListOf()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        adapter = RadioRecyclerAdapter(layoutInflater) {
-            val intent = Intent(this, RadioActivity::class.java)
-            intent.putExtra("title", it.title)
-            intent.putExtra("logo", it.logo)
-            intent.putExtra("stream", it.stream)
-            startActivity(intent)
-        }
-
-        binding.list.adapter = adapter
-        binding.list.layoutManager = LinearLayoutManager(this)
+//        val navController = binding.fragment.findNavController()
+        val navController = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        binding.bottomNav.setupWithNavController(navController.navController)
     }
-
-    override fun onStart() {
-        super.onStart()
-
-        Fuel.get("https://radioplayer-api.herokuapp.com/api/radio").responseString { result ->
-            val json = result.get()
-            val typeToken = object : TypeToken<List<Radio>>() {}.type
-            val radios = Gson().fromJson<List<Radio>>(json, typeToken)
-
-            list.clear()
-            list += radios
-
-            adapter.submitList(list.toList())
-        }
-    }
-
-    data class Radio(val id: Int, val title: String, val stream: String, val logo: String = "")
 }
