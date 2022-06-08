b10170e75aa1e2e212395f0b91055e6358f67a33
yurdmitriev <yurdmitriev02@gmail.com>
2022-06-09 01:30:50 +0300

    Initial implementation of google auth

--- app/src/main/java/com/yurdm/radioplayer/MainActivity.kt
+++ app/src/main/java/com/yurdm/radioplayer/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.yurdm.radioplayer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.yurdm.radioplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +21,22 @@ class MainActivity : AppCompatActivity() {
 
         val fragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         binding.bottomNav.setupWithNavController(fragment.navController)
+
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.server_client_id))
+            .requestEmail()
+            .build()
+
+        val signInClient = GoogleSignIn.getClient(this, gso)
+        signInIntent = signInClient.signInIntent
+
+        account = GoogleSignIn.getLastSignedInAccount(this);
+    }
+
+    companion object {
+        lateinit var gso: GoogleSignInOptions
+        lateinit var signInIntent: Intent
+        var account: GoogleSignInAccount? = null
+        const val RC_SIGN_IN = 1
     }
 }
--- app/src/main/java/com/yurdm/radioplayer/ProfileFragment.kt
+++ app/src/main/java/com/yurdm/radioplayer/ProfileFragment.kt
@@ -1,17 +1,80 @@
 package com.yurdm.radioplayer
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.yurdm.radioplayer.MainActivity.Companion.RC_SIGN_IN
+import com.yurdm.radioplayer.MainActivity.Companion.account
+import com.yurdm.radioplayer.MainActivity.Companion.signInIntent
+import com.yurdm.radioplayer.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.signInButton.setOnClickListener {
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
+        binding.logoutBtn.setOnClickListener {
+            val signInClient = GoogleSignIn.getClient(inflater.context, MainActivity.gso)
+            signInClient.signOut().addOnCompleteListener {
+                account = null
+                updateUI()
+            }
+        }
+
+        updateUI()
+
+        return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                account = task.getResult(ApiException::class.java)
+                updateUI()
+            } catch (e: ApiException) {
+                Toast.makeText(context, "Auth failed!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun updateUI() {
+        if (account == null) {
+            binding.authorized.visibility = View.GONE
+            binding.unauthorized.visibility = View.VISIBLE
+//            binding.authorized.alpha = 0F
+//            binding.unauthorized.alpha = 1F
+        } else {
+            binding.authorized.visibility = View.VISIBLE
+            binding.unauthorized.visibility = View.GONE
+//            binding.authorized.alpha = 1F
+//            binding.unauthorized.alpha = 0F
+            binding.userName.text = account?.displayName
+            binding.userEmail.text = account?.email
+            Glide.with(this).load(account?.photoUrl).fitCenter().into(binding.userImage)
+        }
     }
 }
